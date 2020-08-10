20d9c13876414772cb9abd23d48b0f10cec1380f
Greg Mall <gregmall157@gmail.com>
2020-08-10 07:59:52 -0700

    some style changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
     BrowserRouter as Router,
     Route,
     Switch,
-    Link,
+    
 } from 'react-router-dom';
 import './App.css';
 import SearchPoke from './search/SearchPoke.js';
@@ -25,16 +25,7 @@ return (
 
       <Router>
         <Header />
-          {/* <header>
-            <ul>
-              <li className="title">
-                POKEMON!
-              </li>
-              <li>
-                  <Link to="/">Home</Link>
-              </li>
-              </ul>
-          </header> */}
+     
           <Switch>
             <div className="box">
               <Route 
--- src/detail/Detail.js
+++ src/detail/Detail.js
@@ -7,17 +7,17 @@ export default class Detail extends Component {
 
     componentDidMount= async () =>{
         const name = this.props.match.params.myPokemonId;
-        console.log(name)
+        
         const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${name}`);
 
         const pokemon = data.body.results[0];
-        console.log(pokemon.pokemon)
+        
         this.setState({pokemon: pokemon})
     }
 
-
     render() {
         const {pokemon} = this.state;
+        console.log(pokemon)
 
         return (
             <div className="detail">
@@ -25,22 +25,30 @@ export default class Detail extends Component {
                     pokemon 
                     ? <div className="tooltip">
                         <h2>HOVER OVER FOR MORE INFO!</h2>
-                        <p>{pokemon.pokemon}</p>
-                        <p>Attack: {pokemon.attack}</p>
-                        <p>Defense: {pokemon.defense}</p>
-                        <p>Hidden Ability: {pokemon.ability_hidden}</p>
+                
                         <img src={pokemon.url_image} alt={pokemon.pokemon} />
-                            <div className="tooltiptext">
+                            <span className="tooltiptext">
+                                <p>Name: {pokemon.pokemon}</p>
+                                <p>Attack: {pokemon.attack}</p>
+                                <p>Special Attack: {pokemon.special_attack}</p>
+                                <p>Defense: {pokemon.special_defense}</p>
+                                <p>Special Defense: {pokemon.defense}</p>
+                                <p>Ability 1: {pokemon.ability_1}</p>
+                                <p>Ability 2: {pokemon.ability_2}</p>
+                                <p>Hidden Ability: {pokemon.ability_hidden}</p>
                                 <p>Height: {pokemon.height}</p>
+                                <p>Weight: {pokemon.weight}</p>
                                 <p>Shape: {pokemon.shape}</p>
                                 <p>Speed: {pokemon.speed}</p>
                                 <p>Hit points: {pokemon.hp}</p>
-                                <p>Weight: {pokemon.weight}</p>
+                                <p>Pokebase: {pokemon.pokebase}</p>
                                 <p>Egg Group 1: {pokemon.egg_group_1}</p>                 
-                                <p>Egg Group 2: {pokemon.egg_goup_2}</p> 
+                                <p>Egg Group 2: {pokemon.egg_group_2}</p> 
                                 <p>Base Experience: {pokemon.base_experience}</p>
-                                <p>Ability 1: {pokemon.ability_1}</p>
-                            </div> 
+                                <p>Type 1: {pokemon.type_1}</p>
+                                <p>Type 2:  {pokemon.type_2}</p>
+                                
+                            </span> 
                     </div>
                    
                         : <h2>LOADING</h2>
